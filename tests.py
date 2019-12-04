d3b4453958a42a6afdef856dee7633165bf33363
Felipe Sasso <felipecsasso@gmail.com>
2019-12-04 10:44:34 -0800

    Added new tests for new challenge

--- tests.py
+++ tests.py
@@ -1,20 +1,39 @@
 import unittest
 
 from challenges.v_challenges import visits_on_circular_road
+from challenges.m_challenges import max_zeros
+
 
 class TestVisitsOnCircularRoad(unittest.TestCase):
-
     def test_1(self):
-        n=4
-        visits_order=[1, 3, 2, 3, 1]
+        n = 4
+        visits_order = [1, 3, 2, 3, 1]
         result = visits_on_circular_road(n, visits_order)
         self.assertEqual(result, 6)
 
     def test_2(self):
-        n=3
-        visits_order=[3]
+        n = 3
+        visits_order = [3]
         result = visits_on_circular_road(n, visits_order)
         self.assertEqual(result, 1)
 
-if __name__ == '__main__':
+
+class TestMaxZeros(unittest.TestCase):
+    def test_1(self):
+        n = 9
+        result = max_zeros(n)
+        self.assertEqual(result, 2)
+
+    def test_2(self):
+        n = 7
+        result = max_zeros(n)
+        self.assertEqual(result, 7)
+
+    def test_2(self):
+        n = 127
+        result = max_zeros(n)
+        self.assertEqual(result, 5)
+
+
+if __name__ == "__main__":
     unittest.main()
