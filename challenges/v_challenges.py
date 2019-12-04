1ee81893034963fda33d0c4129eddd94c123328f
Felipe Sasso <felipecsasso@gmail.com>
2019-12-04 10:44:12 -0800

    Added docstring

--- challenges/v_challenges.py
+++ challenges/v_challenges.py
@@ -1,4 +1,10 @@
 def visits_on_circular_road(n, visits_order):
+    """ There are n houses in a village on a circular road numbered from 1 to n
+        starting from some house in clockwise order. It takes one minute to get
+        from one house to any of two adjacent houses and there are no other
+        roads in this village besides the circular one. Find the minimal time
+        required to make all visits in the desired order starting from house 1.
+    """
     res = 0
     pre = 1
     for i in range(len(visits_order)):
