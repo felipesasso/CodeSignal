21395d40f6e216c09dd59edf57d86bf937fc180c
Felipe Sasso <felipecsasso@gmail.com>
2019-12-10 14:53:11 -0800

    Added new unit test

--- test_interview_practice.py
+++ test_interview_practice.py
@@ -1,18 +1,29 @@
 import unittest
 
 from interview_practice.arrays.main import first_duplicate
+from interview_practice.common_techniques.main import contains_duplicates
 
-class FirstDuplicate(unittest.TestCase):
+class TestArrays(unittest.TestCase):
 
-    def test_1(self):
+    def test_first_duplicate_1(self):
         a = [2, 1, 3, 5, 3, 2]
         result = first_duplicate(a)
         self.assertEqual(result, 3)
 
-    def test_2(self):
+    def test_first_duplicate_2(self):
         a = [2, 2]
         result = first_duplicate(a)
         self.assertEqual(result, 2)
 
+class TestCommonTechniques(unittest.TestCase):
+    def test_contains_duplicates_1(self):
+        a=[1, 2, 3, 1]
+        result = contains_duplicates(a)
+        self.assertTrue(result)
+
+    def test_contains_duplicates_2(self):
+        a=[3, 1]
+        result = contains_duplicates(a)
+        self.assertFalse(result)
 if __name__ == '__main__':
     unittest.main()
